edfbb816552f4aaa4838a24c66342994ccdde9c9
Емил Стойчев <estallinone@gmail.com>
2023-07-05 17:55:33 +0300

    project finished

--- pages/components/guestComp.tsx
+++ pages/components/guestComp.tsx
@@ -1,7 +1,12 @@
+import styles from './taskTemplate.module.css'
+
 export const GuestComp = () => {
   return (
     <>
-      <h2>You must login to create and see tasks!</h2>
+      <h3 className={styles.mainHeader}>Welcome to User Tasks App! <br /> here you can easily and quickly create, edit and track your tasks for the day!</h3>
+      <h4 className={styles.mainHeader}>You must login to create and see tasks!</h4>
+
+      <img id={styles.backgroundImage} src="https://images.ctfassets.net/rz1oowkt5gyp/1IgVe0tV9yDjWtp68dAZJq/36ca564d33306d407dabe39c33322dd9/TaskManagement-hero.png" alt="backgroundImage" />
     </>
   );
 };
